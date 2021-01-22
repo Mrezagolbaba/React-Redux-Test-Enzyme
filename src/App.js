77dfd0abcbe93bbc372c1cc27f8312b2cdb1003c
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-01-22 10:38:39 +0330

    add headline to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import  Header from "./component/header";
 import "./app.scss";
+import Headline from "./component/headline";
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Headline header="posts" desc='click on button to see post'/>
     </div>
   );
 }
