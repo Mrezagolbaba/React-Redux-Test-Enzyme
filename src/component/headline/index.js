acfd7f81ec84cda68b3daf73870a130a3f350b59
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-01 18:37:02 +0330

    add class fo headline

--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -1,12 +1,12 @@
 import PropTypes, { object } from "prop-types";
-
+import "./styles.scss";
 const Headline = (props)=>{
     
     if(!props.header){
         return null
     }
     return(       
-            <div data-test="headlineComponent">
+            <div className="headline" data-test="headlineComponent">
                 <h1 data-test='header'>{props.header}</h1>
                 <p data-test='decs'>{props.desc}</p>
             </div>
