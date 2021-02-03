bb4899cdab694135839db0f03fe4b019674ec291
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-03 11:59:16 +0330

    add button component

---
+++ src/component/button/index.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+
+function SharedButton(){
+
+
+    return(
+        <button>
+
+        </button>
+    )
+}
+
+SharedButton.prototype = {
+
+};
+
+export default SharedButton;
