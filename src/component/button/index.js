e82e1ed1e75e1f26d2bab66dabeddf4b1ab0b2f1
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-11 14:03:21 +0330

    update proptypes of component

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -1,18 +1,27 @@
 import PropTypes from "prop-types";
 
 
-function SharedButton(){
+function SharedButton(props){
 
+   function submitEvent(){
+        if(props.emitEvent){
+            props.emitEvent()
+        }
+
+    }
+
+    const {buttonText}= props
 
     return(
-        <button>
-
+        <button onClick={()=>submitEvent()} data-test="buttonComponent">
+            {buttonText}
         </button>
     )
 }
 
 SharedButton.prototype = {
-
+    buttonText: PropTypes.string,
+    emitEvent: PropTypes.func
 };
 
 export default SharedButton;
