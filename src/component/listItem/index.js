f9ebf41a8793f989e40a7357b1518f2818b7abe4
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-14 16:08:08 +0330

    add list items

---
+++ src/component/listItem/index.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+
+function ListItem(params) {
+    
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+ListItem.propTypes = {
+    title: PropTypes.string,
+    desc:PropTypes.string,
+}
+
+export default ListItem;
